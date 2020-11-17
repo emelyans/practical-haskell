bb2fa22d6af454388c297e7320eb0991162953a0
Sergei Emelyanenkov <emelyans@users.noreply.github.com>
2020-11-17 15:37:29 -0500

    Addes some simple functions for Chapter 2.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,8 @@ module Lib
     ( someFunc
     ) where
 
+import Chapter2.SimpleFunctions
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+--someFunc = putStrLn (firstOrEmpty ["abc", "def"])
+someFunc = putStrLn (show (reverse2 [1,2,3,4,5]))
